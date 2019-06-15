b5636c06e8da0b67fe8fa1b4250a9de298165f39
ktpr1223214 <michael.pero1223@gmail.com>
2019-06-15 16:01:36 +0900

    refactor oauth client

--- infra/authentication/examples/oauth/cmd/client/main.go
+++ infra/authentication/examples/oauth/cmd/client/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/google/go-querystring/query"
 	"github.com/pkg/errors"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
@@ -12,22 +16,23 @@ import (
 
 // OAuth Client 側の情報
 const (
-	port     = 9001
+	port     = 9000
 	clientID = "oauth-client-1"
 	// もちろん実際にこういう風に書いては駄目
 	clientSecret = "oauth-client-secret-1"
 	redirectURI  = "http://localhost:9000/callback"
+	userAgent    = "oauth-client-1"
 )
 
 // 認可サーバ側の構成情報
 // 認可のやりとりを行うために、クライアントも知る必要がある
 const (
-	authorizationEndpoint = "http://localhost:9001/authorize"
-	tokenEndpoint         = "http://localhost:9001/token"
+	authorizationURL      = "http://localhost:9001"
+	authorizationEndpoint = "authorize"
+	tokenEndpoint         = "token"
 )
 
 func addOptions(s string, opt interface{}) (string, error) {
-	// reflect.ValueOf(~).Kind() で変数に格納されている値の種類を得る
 	v := reflect.ValueOf(opt)
 	if v.Kind() == reflect.Ptr && v.IsNil() {
 		return s, nil
@@ -44,29 +49,12 @@ func addOptions(s string, opt interface{}) (string, error) {
 	return u.String(), nil
 }
 
-// buildURL
-// options: クエリパラメータの組
-func buildURL(base string, options interface{}) (string, error) {
-	// Parse parses rawurl into a URL structure
-	/*u, err := url.Parse(base)
-	if err != nil {
-		return "", err
-	}*/
-
-	u, err := addOptions(base, options)
-	if err != nil {
-		return "", err
-	}
-
-	return u, nil
-}
-
-func buildRedirect(r *http.Request) {
-
-}
-
 type AuthClient struct {
-	router *http.ServeMux
+	router     *http.ServeMux
+	httpClient *http.Client
+
+	userAgent        string
+	authorizationURL *url.URL
 }
 
 // Run server 実行
@@ -77,6 +65,59 @@ func (a *AuthClient) Run(port int) error {
 	return nil
 }
 
+// newRequest 認可サーバに対するリクエストを行う
+func (a *AuthClient) newRequest(method string, urlPath string, body interface{}) (*http.Request, error) {
+	u, err := a.authorizationURL.Parse(urlPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var buf io.ReadWriter
+	if body != nil {
+		buf = new(bytes.Buffer)
+		err := json.NewEncoder(buf).Encode(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	req, err := http.NewRequest(method, u.String(), buf)
+	if err != nil {
+		return nil, err
+	}
+	if body != nil {
+		req.Header.Set("Content-Type", "application/json")
+	}
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("User-Agent", a.userAgent)
+	return req, nil
+}
+
+// Do
+func (a *AuthClient) do(ctx context.Context, req *http.Request, v interface{}) (*http.Response, error) {
+	req = req.WithContext(ctx)
+	resp, err := a.httpClient.Do(req)
+	if err != nil {
+		// ctx.Done() は channel(<- chan)を返す
+		select {
+		case <-ctx.Done():
+			return nil, errors.Wrap(ctx.Err(), "failed by context")
+		default:
+		}
+		return nil, errors.Wrap(err, "failed to request")
+	}
+	defer resp.Body.Close()
+	// TODO: ここは恐らくもっと拡張すべき
+	if resp.StatusCode != 200 {
+		return nil, errors.Errorf("request failed(status code: %d status: %s)", resp.StatusCode, resp.Status)
+	}
+	err = json.NewDecoder(resp.Body).Decode(v)
+
+	return resp, errors.Wrap(err, "failed to decode response Body")
+}
+
+// 最初の認可リクエスト
+// 認可サーバにリダイレクトを行う
 func (a *AuthClient) auth() http.HandlerFunc {
 	type AuthOption struct {
 		ResponseType string `url:"response_type"`
@@ -90,12 +131,18 @@ func (a *AuthClient) auth() http.HandlerFunc {
 	}
 
 	return func(w http.ResponseWriter, r *http.Request) {
-		authorizationURL, err := buildURL(authorizationEndpoint, opt)
+		urlPath, err := addOptions(authorizationEndpoint, opt)
 		if err != nil {
-			log.Fatal(err)
+			log.Printf("[Error] Failed to build authorization URL %s", err)
+			return
+		}
+		u, err := a.authorizationURL.Parse(urlPath)
+		if err != nil {
+			log.Printf("[Error] Failed to build authorization URL %s", err)
+			return
 		}
 
-		http.Redirect(w, r, authorizationURL, 302)
+		http.Redirect(w, r, u.String(), 302)
 	}
 }
 
@@ -104,8 +151,13 @@ func (a *AuthClient) Routes() {
 }
 
 func main() {
+	au, _ := url.ParseRequestURI(authorizationURL)
+
 	a := &AuthClient{
-		router: http.NewServeMux(),
+		router:           http.NewServeMux(),
+		httpClient:       http.DefaultClient,
+		userAgent:        userAgent,
+		authorizationURL: au,
 	}
 	a.Routes()
 
